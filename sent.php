5ea1e1b3c10035149bd9a5c1deb64079230f21dc
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-05 18:14:25 +0900

    フォーム完成

--- sent.php
+++ sent.php
@@ -24,12 +24,14 @@
       <div class="form-title">入力内容</div>
 
       <div class="form-item">■ 名前</div>
-      <?php echo $_POST["name"]?>
+          <?php echo $_POST["name"]?>
       <div class="form-item">■ 年齢</div>
-      <!-- ageを受け取りechoしましょう --> 
-      <?php echo $_POST["age"] ?>
+          <?php echo $_POST["age"] ?>
+      <div class="form-item">■ お問い合わせの種類</div>
+      <!-- この下でcategoryを受け取りechoしましょう -->
+          <?php echo $_POST["category"]?>
       <div class="form-item">■ 内容</div>
-      <?php echo $_POST["body"]?>
+          <?php echo $_POST["body"]?>
     </div>
 
   </div>
