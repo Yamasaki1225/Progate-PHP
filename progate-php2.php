61b72f46c1a398831bb54e457830bb29a89cf7dd
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-11 18:41:04 +0900

    メソッド

--- progate-php2.php
+++ progate-php2.php
@@ -1,20 +1,25 @@
 <?php
 class Menu {
-// $nameというプロパティを定義してください
-  public $name;  
+  public $name;
+  
+  // helloメソッドを定義してください
+  public function hello() {
+    echo "私はMenuクラスのインスタンスです";
+  }
+  
+  
 }
+
 $curry = new Menu();
 $pasta = new Menu();
-// $curryのnameプロパティに'CURRY'を代入してください
 $curry->name = 'CURRY';
-// $pastaのnameプロパティに'PASTA'を代入してください
 $pasta->name = 'PASTA';
-// $curryのnameプロパティをechoしてください
-echo $curry->name;
+// $curryに対してhelloメソッドを呼び出してください
+$curry->hello();
+
 echo '<br>';
-// $pastaのnameプロパティをechoしてください
-echo $pasta->name;
-echo '<br>';
+// $pastaに対してhelloメソッドを呼び出してください
+$pasta->hello();
 
 
 ?>
