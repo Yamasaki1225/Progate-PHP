c8891923f3e5f75a2cd939b9995c71496e47ffec
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-11 18:23:30 +0900

    プロパティ

--- progate-php2.php
+++ progate-php2.php
@@ -1,13 +1,20 @@
 <?php
-// Menuクラスを定義しましょう
 class Menu {
-  
+// $nameというプロパティを定義してください
+  public $name;  
 }
-
-// Menuクラスのインスタンスを作り、変数$curryに代入してください
 $curry = new Menu();
-
-// Menuクラスのインスタンスを作り、変数$pastaに代入してください
 $pasta = new Menu();
+// $curryのnameプロパティに'CURRY'を代入してください
+$curry->name = 'CURRY';
+// $pastaのnameプロパティに'PASTA'を代入してください
+$pasta->name = 'PASTA';
+// $curryのnameプロパティをechoしてください
+echo $curry->name;
+echo '<br>';
+// $pastaのnameプロパティをechoしてください
+echo $pasta->name;
+echo '<br>';
+
 
 ?>
