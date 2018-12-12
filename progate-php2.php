b4c93b6ecf8704b997402203b2919821a9980727
Yamasaki1225 <yamasaki1600@yahoo.co.jp>
2018-12-12 11:01:16 +0900

    $this

--- progate-php2.php
+++ progate-php2.php
@@ -2,24 +2,19 @@
 class Menu {
   public $name;
   
-  // helloメソッドを定義してください
   public function hello() {
-    echo "私はMenuクラスのインスタンスです";
+    // '私は○○です'とechoしてください
+    echo '私は'.$this->name.'です';
+    
   }
-  
-  
 }
 
 $curry = new Menu();
 $pasta = new Menu();
 $curry->name = 'CURRY';
 $pasta->name = 'PASTA';
-// $curryに対してhelloメソッドを呼び出してください
 $curry->hello();
-
 echo '<br>';
-// $pastaに対してhelloメソッドを呼び出してください
 $pasta->hello();
-
 
 ?>
